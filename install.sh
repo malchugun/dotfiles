c052b356e2ea5a5ed6f71881f184f6db99553c27
malchugun <malchugun@gmail.com>
2018-02-18 23:55:15 +0300

    install fix for vim plugins

--- install.sh
+++ install.sh
@@ -25,5 +25,11 @@ sudo ln -s /usr/bin/python3 /usr/bin/python
 ln -s ~/.dotfiles/vim/.vim ~/.vim
 ln -s ~/.dotfiles/vim/.vim/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 ~/.vim/bundle/YouCompleteMe/./install.py --clang-completer
+
+# changing default shell
+echo "Enter password to change default user shell"
+chsh -s /bin/zsh
+
 
