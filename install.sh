0eb11b091576d37f35bb06967ec6922565974a16
malchugun <malchugun@gmail.com>
2018-02-18 23:45:27 +0300

    install file fix

--- install.sh
+++ install.sh
@@ -12,11 +12,8 @@ ln -s ~/.ddotfiles/git/.gitconfig ~/.gitconfig
 # tmux
 ln -s ~/.ddotfiles/tmux/.tmux-ide.conf ~/.tmux-ide.conf
 
-# vim
-ln -s ~/.ddotfiles/vim/.vim ~/.vim
-ln -s ~/.ddotfiles/vim/.vim/.vimrc ~/.vimrc
-
 # zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/zsh/.oh-my-zsh 
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/.dotfiles/zsh/themes/malchugun.zsh-theme ~/.dotfiles/zsh/.oh-my-zsh/themes/malchugun.zsh-theme
@@ -24,12 +21,9 @@ ln -s ~/.dotfiles/zsh/themes/malchugun.zsh-theme ~/.dotfiles/zsh/.oh-my-zsh/them
 # python
 sudo ln -s /usr/bin/python3 /usr/bin/python
 
-# clonin vundle for vim
+# vim
+ln -s ~/.ddotfiles/vim/.vim ~/.vim
+ln -s ~/.ddotfiles/vim/.vim/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# clonning oh-my-zsh 
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/zsh/.oh-my-zsh 
-
-# install youcompleteme
 ~/.vim/bundle/YouCompleteMe/./install.py --clang-completer
 
