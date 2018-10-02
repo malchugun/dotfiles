9a1ceed93d4a3529bd64e06944b2715554be7c57
vspirin <vspirin77@gmail.com>
2018-10-02 23:42:38 +0300

    change install scrpit names

--- install.sh
+++ install.sh
@@ -3,15 +3,6 @@
 # install necessary packages
 sudo apt install zsh mtr-tiny tcpdump vim git htop iptraf-ng pv lzop tmux ack-grep 
 
-# install packages for youcompleteme
-sudo apt install -y build-essential cmake
-
-# install python packages
-sudo apt install -y python3-pip python3-dev
-if dpkg -l|grep -q python2.7; then sudo apt install -y python-dev; fi
-# via pip3
-pip3 install autopep8 virtualenv
-
 # git
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
@@ -19,24 +10,19 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/tmux/.tmux-ide.conf ~/.tmux-ide.conf
 
 # zsh
-git submodule init
-git submodule update
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/zsh/.oh-my-zsh 
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/.dotfiles/zsh/themes/malchugun.zsh-theme ~/.dotfiles/zsh/.oh-my-zsh/themes/malchugun.zsh-theme
 
-# python
-sudo ln -s /usr/bin/python3 /usr/bin/python
-
 # vim
 ln -s ~/.dotfiles/vim/.vim ~/.vim
-ln -s ~/.dotfiles/vim/.vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/.vim/.vimrc_no_ycm ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-~/.vim/bundle/YouCompleteMe/./install.py --clang-completer
 
 # changing default shell
-echo "\nEnter password to change default user shell\n"
+echo -e "\n Enter password to change default user shell \n"
 chsh -s /bin/zsh
 /bin/zsh
 
