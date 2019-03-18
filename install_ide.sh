735c66b66f685407998b1701744309b9ff9a1617
vspirin <vspirin77@gmail.com>
2019-03-19 00:26:51 +0300

    test for go install

--- install_ide.sh
+++ install_ide.sh
@@ -9,8 +9,25 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 # tmux
 ln -s ~/.dotfiles/tmux/.tmux-ide.conf ~/.tmux-ide.conf
 
-# Go env
+# Go Install
+
+# Download Latest Go
+GOURLREGEX='https://dl.google.com/go/go[0-9\.]+\.linux-amd64.tar.gz'
+echo "Finding latest version of Go for AMD64..."
+url="$(wget -qO- https://golang.org/dl/ | grep -oP 'https:\/\/dl\.google\.com\/go\/go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 )"
+latest="$(echo $url | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2 )"
+echo "Downloading latest Go for AMD64: ${latest}"
+wget --quiet --continue --show-progress "${url}"
+unset url
+unset GOURLREGEX
+# Remove Old Go
+sudo rm -rf /usr/local/go
+# Install new Go
+sudo tar -C /usr/local -xzf go"${latest}".linux-amd64.tar.gz
+echo "Create the skeleton for your local users go directory"
 mkdir -p ~/Projects/Go
+# Remove Download
+rm go"${latest}".linux-amd64.tar.gz
 
 # zsh
 git submodule init
@@ -24,6 +41,7 @@ ln -s ~/.dotfiles/vim/.vim ~/.vim
 ln -s ~/.dotfiles/vim/.vim/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+vim +GoInstallBinaries
 
 # changing default shell
 echo "\nEnter password to change default user shell\n"
