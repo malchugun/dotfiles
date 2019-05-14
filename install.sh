60114bf504979894149d52fd01d9e3f283dc2140
vspirin <vspirin77@gmail.com>
2019-05-14 18:02:00 +0300

    Added remove go install from base installation

--- install.sh
+++ install.sh
@@ -17,25 +17,6 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 # tmux
 ln -s ~/.dotfiles/tmux/.tmux-ide.conf ~/.tmux-ide.conf
 
-# Go Install
-
-# Download Latest Go
-GOURLREGEX='https://dl.google.com/go/go[0-9\.]+\.linux-amd64.tar.gz'
-echo "Finding latest version of Go for AMD64..."
-url="$(wget -qO- https://golang.org/dl/ | grep -oP 'https:\/\/dl\.google\.com\/go\/go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 )"
-latest="$(echo $url | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2 )"
-echo "Downloading latest Go for AMD64: ${latest}"
-wget --quiet --continue --show-progress "${url}"
-unset url
-unset GOURLREGEX
-# Remove Old Go
-sudo rm -rf /usr/local/go
-# Install new Go
-sudo tar -C /usr/local -xzf go"${latest}".linux-amd64.tar.gz
-echo "Create the skeleton for your local users go directory"
-mkdir -p ~/Projects/Go
-# Remove Download
-rm go"${latest}".linux-amd64.tar.gz
 # For current shell
 export GOPATH=$HOME/Projects/Go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
