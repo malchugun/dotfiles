05fe53b061502b7c1cea4ef201c33adc21192d97
vspirin <vspirin77@gmail.com>
2019-11-23 11:46:28 +0300

    Added run with go

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/malchugun/dotfiles.git ~/.dotfiles
 apt install screen
-screen -d -m -S bash ~/.dotfiles/install.sh
+screen -d -m -S bash ~/.dotfiles/install_with_GO.sh
 
 
