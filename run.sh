ba418ec66480d0945ce50634374a18dbf646822b
vspirin <vspirin77@gmail.com>
2019-12-06 11:08:34 +0300

    Removed sudo for screen

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/malchugun/dotfiles.git ~/.dotfiles
 sudo apt install screen
-sudo screen -d -m -S bash ~/.dotfiles/install.sh
+screen -d -m -S bash ~/.dotfiles/install.sh
 
 
