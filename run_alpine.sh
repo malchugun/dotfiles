b3f3458fe9ac9c4ce5520412ec99338474044b46
Спирин Владимир <v.spirin@tendertech.ru>
2021-03-20 12:39:18 +0300

    Fix

--- run_alpine.sh
+++ run_alpine.sh
@@ -2,6 +2,6 @@
 
 apk add git screen curl
 git clone https://github.com/malchugun/dotfiles.git ~/.dotfiles
-screen -d -m -S bash ~/.dotfiles/install_on_alpine.sh
+screen -d -m -S ash ~/.dotfiles/install_on_alpine.sh
 
 
