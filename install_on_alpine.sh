326353adb47540077de001557380dc22d11125b3
Спирин Владимир <v.spirin@tendertech.ru>
2021-03-20 13:47:46 +0300

    Added shadow to alpine installation

--- install_on_alpine.sh
+++ install_on_alpine.sh
@@ -7,7 +7,7 @@ sudo apk add vim
 sudo apk add python3
 
 # install necessary packages
-sudo apk add zsh mtr tcpdump git htop iptraf-ng pv lzo tmux ack 
+sudo apk add zsh mtr tcpdump git htop iptraf-ng pv lzo tmux ack shadow
 
 # git
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
