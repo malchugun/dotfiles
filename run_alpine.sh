f802b60c2d9e9603501c3020f3dbbfc386439b9e
Спирин Владимир <v.spirin@tendertech.ru>
2021-03-20 12:41:07 +0300

    Added sudo

--- run_alpine.sh
+++ run_alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-apk add git screen curl
+apk add git screen curl sudo
 git clone https://github.com/malchugun/dotfiles.git ~/.dotfiles
 screen -d -m -S ash ~/.dotfiles/install_on_alpine.sh
 
